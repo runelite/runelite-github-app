e0b893ebbdba947050972a724bcf190829c07ffe
Max Weber <mii7303@gmail.com>
2019-12-12 11:27:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+![](https://runelite.net/img/logo.png)
+# runelite-discord-bot [![Discord](https://img.shields.io/discord/301497432909414422.svg)](https://discord.gg/mePCs8U)
+
+https://probot.github.io/docs/configuration/
+
+Add a github team to the .env
+```
+TEAM=12345654
+```
+
+If your app cannot access the team, add a personal api key with it
+```
+TEAM_TOKEN=bk15b981ubd4jbd31b958j154f
+```
+
+Run
+```
+npm install
+npm start
+```
---
+++ index.ts
@@ -0,0 +1,118 @@
+import { Application, Octokit } from 'probot'
+
+const PLUGIN_CHANGE = "plugin change";
+const READY_TO_MERGE = "ready to merge";
+
+const TEAM = ~~process.env.TEAM!;
+
+// If you run the app outside of an org it can't get to the org's teams
+let globalTeamGithub : Octokit | undefined;
+if (process.env.TEAM_TOKEN) {
+	globalTeamGithub = new Octokit({auth: process.env.TEAM_TOKEN});
+}
+
+export = (app: Application) => {
+	app.on(['pull_request.opened', 'pull_request.synchronized', 'pull_request.reopened'], async context => {
+		const github = context.github;
+
+		let { data: labelList } = await github.issues.listLabelsOnIssue(context.issue());
+		let labels = new Set(labelList.map(l => l.name));
+
+		if (labels.has(READY_TO_MERGE)) {
+			github.issues.removeLabel(context.issue({ name: READY_TO_MERGE }));
+		}
+
+		let files = (await github.pulls.listFiles(context.issue()))
+			.data
+			.filter(f => f.filename.startsWith("plugins/"));
+		if (files.length < 0) {
+			return;
+		}
+
+		let difftext = (await Promise.all(files.map(async file => {
+			let pluginName = file.filename.replace("plugins/", "");
+			if (file.status == "removed") {
+				return `Removed \`${pluginName}\` plugin`;
+			}
+			let readKV = (res: Octokit.Response<string>) => res.data.split("\n")
+				.map(i => /([^=]+)=(.*)/.exec(i))
+				.filter(i => i)
+				.reduce((acc: { [key: string]: string }, val) => {
+					acc[val![1]] = val![2];
+					return acc;
+				}, {});
+			let newPlugin = readKV(await github.request(file.raw_url));
+
+			let extractURL = (cloneURL: string) => {
+				let urlmatch = /https:\/\/github\.com\/([^/]+)\/([^.]+).git/.exec(cloneURL);
+				if (!urlmatch) {
+					throw `Plugin repository must be a github https clone url, not \`${cloneURL}\``;
+				}
+				let [, user, repo] = urlmatch;
+				return { user, repo };
+			};
+			let { user, repo } = extractURL(newPlugin.repository);
+
+			if (file.status == "modified") {
+				let oldPlugin = readKV(await github.request(`https://github.com/${context.repo().owner}/${context.repo().repo}/raw/master/plugins/${pluginName}`));
+				let oldPluginURL = extractURL(oldPlugin.repository);
+				return `\`${pluginName}\`: https://github.com/${oldPluginURL.user}/${oldPluginURL.repo}/compare/${oldPlugin.commit}...${user}:${newPlugin.commit}`;
+			} else if (file.status == "added") {
+				return `New plugin \`${pluginName}\`: https://github.com/${user}/${repo}/tree/${newPlugin.commit}`;
+			} else {
+				return `What is a \`${file.status}\`?`;
+			}
+		}))).join("\n\n");
+
+		if (difftext && !labels.has(PLUGIN_CHANGE)) {
+			await github.issues.addLabels(context.issue({ labels: [PLUGIN_CHANGE] }));
+		}
+
+		let marker = "<!-- rlphc -->";
+		let body = marker + "\n" + difftext;
+		let sticky = (await github.issues.listComments(context.issue()))
+			.data.find(c => c.body.startsWith(marker));
+		if (sticky) {
+			github.issues.updateComment(context.issue({ comment_id: sticky.id, body }));
+		} else {
+			github.issues.createComment(context.issue({ body }));
+		}
+	});
+
+	app.on(["pull_request_review.submitted"], async context => {
+		const github = context.github;
+		const teamGithub = globalTeamGithub || github;
+
+		let { data: labelList } = await github.issues.listLabelsOnIssue(context.issue());
+		let labels = new Set(labelList.map(l => l.name));
+
+		if (!labels.has(PLUGIN_CHANGE)) return;
+
+		let { data: reviews } = await github.pulls.listReviews(context.issue());
+		if (reviews.length <= 0) return;
+
+		let { data: memberList } = await teamGithub.teams.listMembers({ team_id: TEAM });
+		let members = new Set(memberList.map(m => m.login));
+
+		let reviewStates: { [key: string]: boolean } = {};
+		reviews.filter(r => members.has(r.user.login))
+			.forEach(r => {
+				let approved = r.state == "APPROVED" || r.body == "lgtm";
+				if (approved || (!approved && r.state != "COMMENTED")) {
+					reviewStates[r.user.login] = approved;
+				}
+			});
+		let unapproved = Object.keys(reviewStates).filter(k => !reviewStates[k]);
+
+		if (unapproved.length > 0) {
+			console.log(`Unapproved for #${context.issue().number}: ${unapproved}`);
+			if (labels.has(READY_TO_MERGE)) {
+				await github.issues.removeLabel(context.issue({ name: READY_TO_MERGE }));
+			}
+		} else {
+			if (!labels.has(READY_TO_MERGE)) {
+				await github.issues.addLabels(context.issue({ labels: [READY_TO_MERGE] }));
+			}
+		}
+	});
+}
